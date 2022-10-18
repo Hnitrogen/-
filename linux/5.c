fcaa4aef81e6aafaee26a031a023ca0185cf0c88
Hnitrogen <1791422575@qq.com>
2022-10-19 01:00:41 +0800

    Update 5.c

--- linux/5.c
+++ linux/5.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 
-void *cat(char *s , char *t)
+#define N 100010
+
+void * cat(char *s, char *t)
 {
-	
+    int i , j ;
+
+    while(s[i] != '\0') i++; 
+
+    for(int j = 0 ; t[j] != '\0' ; j++)
+    {
+        s[i] = t[j];
+        i++;
+    }  
+    s[i] = '\0';
 }
 
 int main()
 {
-	char a[100010] , b[100010] ;
+    char s1[N] ,s2[N] ;
+    scanf("%s%s",s1,s2);
+    
+    cat(s1,s2);
+    printf("%s",s1);
 
-	scanf("%s%s",a,b);
-
-	//printf("%s%s",a,b);
-	cat(*a,*b) ;
-
-	return 0 ;
+    return 0 ;
 }
-
