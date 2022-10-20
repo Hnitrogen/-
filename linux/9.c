a0fcd7c6b68bdbbcd9ab87ab7c7a4023263a99b9
Hnitrogen <1791422575@qq.com>
2022-10-21 00:40:47 +0800

    Update 9.c

--- linux/9.c
+++ linux/9.c
@@ -4,25 +4,23 @@
 
 int is_huiwen(const char* str, int length)
 {
-    //str = str.substr(1,length-2)
-    if(length <= 1)
+    if(length == 1)
         return 1;//递归基准出口 
     
-    if(str[0] != str[length-1])
+    if(str[strlen(str) - length] != str[length - 1])
         return 0;//不是回文串 
     else
-		return is_huiwen(substr(str,1,length-2),length-2);//递归调用 
+        return is_huiwen(str,length-1);//递归调用 
 }
 
-// l eve l str.substr  
 int main(){
-	char str[80];
-	gets(str);
-	
-	if(is_huiwen(str,strlen(str)))
-		printf("YES");
-	else
-		printf("NO");
-		
-	return 0;
+ char str[80];
+ gets(str);
+ 
+ if(is_huiwen(str,strlen(str)))
+  printf("YES");
+ else
+  printf("NO");
+  
+ return 0;
 }
